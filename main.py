61edc6603f67491ece380bb2cb6a50c78b7997a9
Chenggang Zhao <lyricz@yeah.net>
2021-12-14 10:49:38 +0800

    Add distribution simulation

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def shift_choice(p: float) -> int:
     return 1 if choice(p) else -1
 
 
-def simulate(a1: int, a2: int, p1: float, p2: float, steps_limit: int = 1000) -> int:
+def simulate(a1: int, a2: int, p1: float, p2: float, steps_limit: int = 100) -> int:
     if a1 == a2:
         return 0
     assert a1 <= a2, 'The point 1 must be on the left of the point 2'
@@ -29,23 +29,29 @@ def simulate(a1: int, a2: int, p1: float, p2: float, steps_limit: int = 1000) ->
     return None
 
 
-def multiple_simulate(bind_func_obj: partial, simulation_times: int = 100000) -> [int]:
-    s = []
+def multiple_simulate(bind_func_obj: partial, simulation_times: int = 100000) -> ([int], [(int, float)]):
+    a, d = [], {}
     assert simulation_times > 0
     for i in range(simulation_times):
         x = None
         while x is None:
             x = bind_func_obj()
-        s.append(x)
-    return s
+        a.append(x)
+        d[x] = d[x] + 1 if x in d else 1
+    d = sorted(d.items())
+    s = sum(map(lambda x: x[1], d))
+    d = list(map(lambda x: (x[0], x[1] / s), d))
+    return a, d
 
 
-def expectation(bind_func_obj: partial, simulation_times: int = 100000) -> float:
-    return np.mean(multiple_simulate(bind_func_obj, simulation_times))
+def expectation(bind_func_obj: partial) -> float:
+    a, _ = multiple_simulate(bind_func_obj)
+    return np.mean(a)
 
 
-def variance(bind_func_obj: partial, simulation_times: int = 100000) -> float:
-    return np.var(multiple_simulate(bind_func_obj, simulation_times), ddof=1)  # should be without bias
+def variance(bind_func_obj: partial) -> float:
+    a, _ = multiple_simulate(bind_func_obj)
+    return np.var(a, ddof=1)  # should be without bias
 
 
 def draw(func, x_label: str, y_label: str, ref_func=None):
@@ -94,3 +100,10 @@ if __name__ == '__main__':
             f = partial(simulate, 0, i, 0.8, 0.2)
             yield i, variance(f)
     draw(e_var_delta_a, 'a_2 - a_1', 'Var[T_c]', lambda a: a * (1 - 0.8) * 0.8 / ((2 * 0.8 - 1) ** 3))
+
+    # Distribution when p = 0.5
+    def t_c_distribution():
+        f = partial(simulate, 0, 4, 0.5, 0.5)
+        a, d = multiple_simulate(f)
+        return d
+    draw(t_c_distribution, 'T_c', 'Distribution')
